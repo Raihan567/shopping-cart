c82f2465ab8e4371206292bb88d2ecacf21c7dd5
Raihan567 <aburaihansarker567@gmail.com>
2022-09-05 19:07:55 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import "./App.css";
+import React from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useThemeHook } from './globalComponents/ThemeProvider';
+import Header from './components/Header';
+
+//Pages
+// import Home from './Pages/Home';
+// import Cart from './Pages/Cart';
+
+
 function App() {
+  const [theme] = useThemeHook();
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <main className={theme? 'bg-black': 'bg-light-2'} style={{ height: '100vh', overflowY: 'auto'}}>
+      <Header/>
+    </main>
   );
 }
 
